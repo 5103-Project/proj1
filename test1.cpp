782395ea031146d29e32b5f49d516b2a1c7a63a3
G-H-Y <1569996795@qq.com>
2019-10-20 23:46:59 +0800

    add three tests case and modified thread.cc

---
+++ test1.cpp
@@ -0,0 +1,69 @@
+#include<iostream>
+#include"thread.h"
+
+using namespace std;
+
+
+void myPause(){
+	int t = 1;
+	for(int i = 0; i < 100000000; i++){
+		t *= i;
+	}
+}
+
+void *f(void*)
+{
+    int i=0;
+    while(1) {
+        ++i;
+        printf("in f (%d)\n",i);
+        myPause();
+    }
+}
+
+void *g(void*)
+{
+    int i=0;
+    while(1){
+        ++i;
+        printf("in g (%d)\n",i);
+        //if (i % 5 == 0) {
+        //    printf("g: switching\n");
+        //}
+        myPause();
+    }
+}
+
+
+
+int main()
+{
+    cout<<"This test case is used to test uthread_init, uthread_create,"<<endl;
+    cout<<"uthread_yeild, scheduler, time slcing and basic context swtich."<<endl;
+    //setup();
+    uthread::uthread_init(2 * MICROSEC);
+
+    Thread *t1 = new Thread();
+    t1->uthread_create(f,NULL);
+    
+    Thread *t2 = new Thread(); 
+    t2->uthread_create(g, NULL);
+
+    // main thread
+    Thread* main_thread = new Thread();
+
+    main_thread->S = RUNNING;
+    uthread::RunningList.push_back(main_thread->tcb);
+    uthread::Threads[0] = main_thread;
+
+    main_thread->tcb->id = 0;
+    sigsetjmp(main_thread->tcb->jbuf,1);
+ 
+
+    while(1){
+    	cout<<"back to main\n";
+    	myPause();
+    }
+
+    return 0;
+}
